db48a3d3e423d7a610e9b83352008ecb3607d8c0
Leonardo Duarte <leoduarteantonio@gamil.com>
2021-10-27 14:42:55 -0600

    Cambios en los procedimientos almacenados de repuestos y mantenimientos

--- Querys/Procedimientos-Selects.sql
+++ Querys/Procedimientos-Selects.sql
@@ -1,3 +1,4 @@
+
 Select * from Usuarios
 Select * from Colaboradores
 select * from Clientes
@@ -96,24 +97,84 @@ alter table [Detalle de Repuestos]
 add Descuento float 
 
 
---Procedimiento mostrar todos los mantenimientos
+--Procedimiento mostrar todos los mantenimientos como reporte 
+ALTER procedure [dbo].[Mostrar_Mantenimientos]
+as
+--Procedimiento mostrar todos los mantenimientos como reporte 
 ALTER procedure [dbo].[Mostrar_Mantenimientos]
 as
 select 
-NoMantenimiento as [No Mantenimiento],
-c.p_nombre+' '+c.p_apellido as [Nombre del Cliente],
-v.Marca as [Marca del vehiculo],
-v.Modelo as [Modelo del vehiculo],
-v.Año as [Año del vehiculo],
+m.NoMantenimiento as [No Mantenimiento],
+c.p_nombre+' '+c.p_apellido as [Cliente],
+v.Marca+', '+ v.Modelo +', '+ CONVERT(nvarchar(50),YEAR(v.Año),0) as [Vehiculo],
+s.Descripcion as Servicio,
+s.Precio as [Costo del servicio],
+s.[Tipo de Mantenimiento],
+r.Titulo+', '+r.Marca+', '+r.Modelo  as Repuesto,
+r.Descripcion [Descripcion de repuesto],
+dr.CostoUnitario as [Costo por repuesto],
+dr.Descuento as [Descuento por repuesto],
+dr.Cantidad as [Cantidad de repuestos],
+m.Estado as [Estado del vehiculo],
 FechaIngreso as [Fecha de ingreso],
 FechaSalida as [Fecha de salida],
-Estado
+me.p_nombre+' '+me.p_apellido as [Mecanico a cargo],
+ROUND(((dr.CostoUnitario*dr.Cantidad)-(dr.Cantidad*dr.Descuento))+(s.Precio),2) as [Costo Total]
 from Mantenimientos m
 inner join Vehiculos v 
 on v.NoVehiculo = m.NoVehiculo
 inner join Clientes c
-on c.NoCliente = v.NoCliente
+on c.NoCliente = v.NoCliente 
+inner join [Detalle de Mantenimiento] dm
+on dm.NoMantenimiento = m.NoMantenimiento
+inner join Servicios s 
+on s.NoServicio = dm.NoServicio
+inner join Repuestos r 
+on r.NoRepuesto = dm.NoRepuesto
+inner join Mecanicos me
+on me.NoMecanico = dm.NoMecanico
+inner join [Detalle de Repuestos] dr
+on dr.NoRepuesto = r.NoRepuesto
 
+/*select 
+m.NoMantenimiento as [No Mantenimiento],
+c.p_nombre+' '+c.p_apellido as [Nombre del Cliente],
+v.Marca as [Marca del vehiculo],
+v.Modelo as [Modelo del vehiculo],
+v.Año as [Año del vehiculo],
+s.Descripcion as Servicio,
+s.Precio,
+s.[Tipo de Mantenimiento],
+r.Titulo as Repuesto,
+r.Descripcion,
+r.Marca as [Marca del Repuesto],
+r.Modelo as [Modelo del Repuesto],
+me.p_nombre+' '+me.p_apellido as [Mecanico a cargo],
+dr.CostoUnitario as [Costo por Unidad],
+dr.Descuento,
+dr.Cantidad,
+FechaIngreso as [Fecha de ingreso],
+FechaSalida as [Fecha de salida],
+m.Estado,
+ROUND(((dr.CostoUnitario*dr.Cantidad)-(dr.Cantidad*dr.Descuento))+(s.Precio),2) as [Costo Total]
+from Mantenimientos m
+inner join Vehiculos v 
+on v.NoVehiculo = m.NoVehiculo
+inner join Clientes c
+on c.NoCliente = v.NoCliente 
+inner join [Detalle de Mantenimiento] dm
+on dm.NoMantenimiento = m.NoMantenimiento
+inner join Servicios s 
+on s.NoServicio = dm.NoServicio
+inner join Repuestos r 
+on r.NoRepuesto = dm.NoRepuesto
+inner join Mecanicos me
+on me.NoMecanico = dm.NoMecanico
+inner join [Detalle de Repuestos] dr
+on dr.NoRepuesto = r.NoRepuesto*/
+
+
+select * from [Detalle de Repuestos]
 select * from Mantenimientos
 select * from [Detalle de Mantenimiento]
 
@@ -150,3 +211,6 @@ ALTER procedure [dbo].[Buscar_Usuario]
 	or Telefono like @dato + '%' 
 	or Rol like @dato + '%' 
 	or Estado  like @dato + '%' 
+
+
+	BACKUP DATABASE	SERVIPLUS TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\SERVIPLUS.bak'
